54ca0def7683b423078b455c25a959773f4fa61f
Lavanya Thiruvali Sunderarajan <lavanyas@skillup.tech>
2021-03-30 20:25:44 +0530

    Added some styling and images

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,14 +1,35 @@
 import './bootstrap.min.css';
 import React from 'react';
+import HomePage from './homepage.js';
+import ContactUs from './contactus.js';
 
-/*
+window.watsonAssistantChatOptions = {
+    integrationID: "f071e502-b562-4c16-ad6d-e2cc94005cd1", // The ID of this integration.
+    region: "eu-gb", // The region your integration is hosted in.
+    serviceInstanceID: "815b0cb8-969a-420f-a759-7ef5016c1b0e", // The ID of your service instance.
+    onLoad: function(instance) { instance.render(); }
+  };
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/loadWatsonAssistantChat.js";
+  document.head.appendChild(t);
+});
 
-
-Paste the Watson Assitant Code here
-
-*/
 
 class App extends React.Component {
+
+  state = {
+    pageshown:<HomePage/>
+  }
+
+  setPageHome = ()=> {
+    this.setState({pageshown:<HomePage/>});    
+  }
+  
+  setPageContactUs = ()=> {
+    this.setState({pageshown:<ContactUs/>});    
+  }
+  
   render() {
     return (
       <div>
@@ -16,22 +37,17 @@ class App extends React.Component {
           <div class="navbar-collapse" id="navbarTogglerDemo01">
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
               <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="#" onClick={this.setPageHome}>Home <span class="sr-only">(current)</span></a>
               </li>
-                {/* 
-                
-                
-                Add more relevant links to tabs here and add code 
-                to make it more functional 
-                
-                
-                */}
               <li class="nav-item">
-                <a class="nav-link" href="#">Contact Us</a>
+                <a class="nav-link" href="#" onClick={this.setPageContactUs}>Contact Us</a>
               </li>
             </ul>
           </div>
         </nav>
+        <div>
+          {this.state.pageshown}
+        </div>
       </div>
     );
   }
---
+++ server/frontend/src/contactus.js
@@ -0,0 +1,23 @@
+import './bootstrap.min.css';
+import React from 'react';
+
+class ContactUs extends React.Component {
+  render() {
+    return (
+      <div>
+        <div style={{display:"flex"}}>
+        <img src='/contatus.jpeg' style={{width: "300px"}}/>
+        <div style={{margin:"100px"}}>
+             <span style={{margin:"60px"}}>
+             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+               </span >
+           </div>
+
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default ContactUs;
