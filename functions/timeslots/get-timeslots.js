ac4681b34d85c2586473f520f58ea252dd9f6d3f
Upkar Lidder <ulidder@us.ibm.com>
2021-04-12 21:17:10 -0700

    updating for webhook

--- functions/timeslots/get-timeslots.js
+++ functions/timeslots/get-timeslots.js
@@ -14,107 +14,51 @@ function main(params) {
   // ideally the code would look this up in the database, but due to limited time, this function returns some hardcoded values at this time
   if (params && params.date) {
     const d = new Date(params.date);
+    // let result = {
+    //   date: params.date
+    // }
+
     let result = {
-      date: params.date
+      "arr": [
+        {
+          "title": `Available time slots for ${params.date}`,
+          "options": [
+          ],
+          "description": "",
+          "response_type": "option"
+        }
+      ]
     }
+
     switch (d.getDay()) {
       case 0:
         // Sunday
-        result.day = "Sunday";
-        result.timeslots = [
-          {
-            "start": "8:00 am",
-            "end": "8:30am"
-          },
-          {
-            "start": "11:00 am",
-            "end": "11:30 am"
-          }
-        ]
+        // result.day = "Sunday";
+        getSlots(result);
         break;
       case 1:
         // Monday
-        result.day = "Monday";
-        result.timeslots = [
-          {
-            "start": "8:00 am",
-            "end": "8:30am"
-          },
-          {
-            "start": "11:00 am",
-            "end": "11:30 am"
-          }
-        ]
+        getSlots(result);
         break;
       case 2:
         // Tue
-        result.day = "Tuesday";
-        result.timeslots = [
-          {
-            "start": "8:00 am",
-            "end": "8:30am"
-          },
-          {
-            "start": "11:00 am",
-            "end": "11:30 am"
-          }
-        ]
+        getSlots(result);
         break;
       case 3:
         // Wed
-        result.day = "Wednesday";
-        result.timeslots = [
-          {
-            "start": "8:00 am",
-            "end": "8:30am"
-          },
-          {
-            "start": "11:00 am",
-            "end": "11:30 am"
-          }
-        ]
+        getSlots(result);
         break;
       case 4:
         // Thu
-        result.day = "Thursday";
-        result.timeslots = [
-          {
-            "start": "8:00 am",
-            "end": "8:30am"
-          },
-          {
-            "start": "11:00 am",
-            "end": "11:30 am"
-          }
-        ]
+        getSlots(result);
         break;
       case 5:
         // Fri
-        result.day = "Friday";
-        result.timeslots = [
-          {
-            "start": "8:00 am",
-            "end": "8:30am"
-          },
-          {
-            "start": "11:00 am",
-            "end": "11:30 am"
-          }
-        ]
+        getSlots(result);
         break;
       case 6:
         // Sat
-        result.day = "Saturday";
-        result.timeslots = [
-          {
-            "start": "8:00 am",
-            "end": "8:30am"
-          },
-          {
-            "start": "11:00 am",
-            "end": "11:30 am"
-          }
-        ]
+        getSlots(result);
         break;
       default:
         result = {
@@ -126,6 +70,35 @@ function main(params) {
   }
 }
 
-console.log(main({ date: '04/02/21' }));
+function getWeekend(result) {
+  //Learner: add code to return the following available times for the weekend:
+  // 7:00am - 9:00am
+  // 2:00pm - 3:00pm
+}
+
+function getSlots(result) {
+  result.arr[0].options.push(
+    {
+      "label": "8:00am - 9:00am",
+      "value": {
+        "input": {
+          "text": "8:00am - 9:00am"
+        }
+      }
+    });
+
+  result.arr[0].options.push({
+    "label": "11:00am - 1:00pm",
+    "value": {
+      "input": {
+        "text": "11:00am - 1:00pm"
+      }
+    }
+  });
+
+  return result;
+}
+
+// console.log(JSON.stringify(main({ date: '04/02/21' })));
 
 exports.main = main;
