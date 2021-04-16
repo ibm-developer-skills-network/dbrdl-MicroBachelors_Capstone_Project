ebec67f62a5783f1f1a8fc611594b2f3eb628c11
Upkar Lidder <ulidder@us.ibm.com>
2021-04-15 20:30:06 -0700

    fixing get reviews

--- functions/details/get-details.js
+++ functions/details/get-details.js
@@ -1,3 +1,5 @@
+var Cloudant = require('@cloudant/cloudant');
+
 /**
   *
   * main() will be run when you invoke this action
@@ -8,135 +10,142 @@
   *
   */
 function main(params) {
-    if(params && params.type === "reviews") {
-        return getReviews();
+  return new Promise((resolve, reject) => {
+    if (params && params.type === "reviews") {
+      getReviews(params).then(reviews => {
+        resolve({"reviews":reviews.docs})
+      });
     } else if (params && params.type === "timeslots") {
-        if(params.date) {
-            return getTimeSlots(params.date);
-        } else {
-                return getTimeSlots(new Date());
-        }
+      if (params.date) {
+        resolve(getTimeSlots(params.date));
+      } else {
+        resolve(getTimeSlots(new Date()));
+      }
     }
-    
+  })
 }
 
-function getReviews() {
-    return {
-        reviews: [
-          {
-            "id": 1,
-            "name": "Berkly Shepley",
-            "dealership": 14,
-            "review": "Total grid-enabled service-desk",
-            "purchase": true,
-            "purchase_date": "07/11/2020",
-            "car_make": "Audi",
-            "car_model": "A6",
-            "car_year": 2010
-          },
-          {
-            "id": 2,
-            "name": "Gwenora Zettoi",
-            "dealership": 14,
-            "review": "Future-proofed foreground capability",
-            "purchase": true,
-            "purchase_date": "09/17/2020",
-            "car_make": "Pontiac",
-            "car_model": "Firebird",
-            "car_year": 1995
-          }
-        ]
-      };
+function getReviews(params) {
+  console.log(params.CLOUDANT_URL);
+  console.log(params.CLOUDANT_APIKEY);
+  let reviews = [];
+  const cloudant = Cloudant({ url: params.CLOUDANT_URL, plugins: { iamauth: { iamApiKey: params.CLOUDANT_APIKEY } } });
+  const db = cloudant.db.use('reviews')
+
+  return db.find({ selector: { dealership: 14 }}) ;
+
+  // db.find({ selector: { dealership: 14 } }, function (err, result) {
+  //   if (err) {
+  //     throw err;
+  //   }
+
+  //   console.log('Found %d documents', result.docs.length);
+  //   console.log(JSON.stringify(result));
+  //   result.docs.forEach(review => {
+  //     console.log('  Doc id: %s', review._id);
+  //     reviews.push(review)
+  //   });
+  // });
+  // return reviews;
 }
 
 
 function getTimeSlots(date) {
 
-    // ideally the code would look this up in the database, but due to limited time, this function returns some hardcoded values at this time
-      console.log("Inga varumbodhu date  "+date);
-      const d = new Date(date);
-  
-      let result = {
-        "arr": [
-          {
-            "title": `Available time slots for ${date}`,
-            "options": [
-            ],
-            "description": "",
-            "response_type": "option"
-          }
-        ]
-      }
-  
-      switch (d.getDay()) {
-        case 0:
-          // Sunday
-          // result.day = "Sunday";
-          getSlots(result);
-          break;
-        case 1:
-          // Monday
-          getSlots(result);
-          break;
-        case 2:
-          // Tue
-          getSlots(result);
-          break;
-        case 3:
-          // Wed
-          getSlots(result);
-          break;
-        case 4:
-          // Thu
-          getSlots(result);
-          break;
-        case 5:
-          // Fri
-          getSlots(result);
-          break;
-        case 6:
-          // Sat
-          getSlots(result);
-          break;
-        default:
-          result = {
-            error: "Something went wrong!"
-          }
-          break;
-      }
-      return result;
-    }
-  
-  
-  
-  function getWeekend(result) {
-    //Learner: add code to return the following available times for the weekend:
-    // 7:00am - 9:00am
-    // 2:00pm - 3:00pm
-  }
-  
-  function getSlots(result) {
-    result.arr[0].options.push(
+  // ideally the code would look this up in the database, but due to limited time, this function returns some hardcoded values at this time
+  console.log("Inga varumbodhu date  " + date);
+  const d = new Date(date);
+
+  let result = {
+    "arr": [
       {
-        "label": "8:00am - 9:00am",
-        "value": {
-          "input": {
-            "text": "8:00am - 9:00am"
-          }
-        }
-      });
-  
-    result.arr[0].options.push({
-      "label": "11:00am - 1:00pm",
+        "title": `Available time slots for ${date}`,
+        "options": [
+        ],
+        "description": "",
+        "response_type": "option"
+      }
+    ]
+  }
+
+  switch (d.getDay()) {
+    case 0:
+      // Sunday
+      // result.day = "Sunday";
+      getSlots(result);
+      break;
+    case 1:
+      // Monday
+      getSlots(result);
+      break;
+    case 2:
+      // Tue
+      getSlots(result);
+      break;
+    case 3:
+      // Wed
+      getSlots(result);
+      break;
+    case 4:
+      // Thu
+      getSlots(result);
+      break;
+    case 5:
+      // Fri
+      getSlots(result);
+      break;
+    case 6:
+      // Sat
+      getSlots(result);
+      break;
+    default:
+      result = {
+        error: "Something went wrong!"
+      }
+      break;
+  }
+  return result;
+}
+
+
+
+function getWeekend(result) {
+  //Learner: add code to return the following available times for the weekend:
+  // 7:00am - 9:00am
+  // 2:00pm - 3:00pm
+}
+
+function getSlots(result) {
+  result.arr[0].options.push(
+    {
+      "label": "8:00am - 9:00am",
       "value": {
         "input": {
-          "text": "11:00am - 1:00pm"
+          "text": "8:00am - 9:00am"
         }
       }
     });
-  
-    return result;
-  }
-  
+
+  result.arr[0].options.push({
+    "label": "11:00am - 1:00pm",
+    "value": {
+      "input": {
+        "text": "11:00am - 1:00pm"
+      }
+    }
+  });
+
+  return result;
+}
+
+// main({
+//   type: "reviews",
+//   CLOUDANT_APIKEY: "vg7UbPIWuF5VgcQckaXlN71OvC-bIS4dImK3QIJfCWFp",
+//   CLOUDANT_URL: "https://923526c1-b1da-4c83-a39f-54372cb71c8d-bluemix.cloudantnosqldb.appdomain.cloud"
+// }).then(response => {
+//   console.log('response');
+//   console.log(response);
+// })
+
 exports.main = main;
 
