990505d631cc4ce6fa29e0c44350ac9e42117823
RomanaAAS <romana.leitgeb247@gmail.com>
2022-07-21 12:12:58 +0000

    added bootstrap touches

--- server/frontend/src/homepage.js
+++ server/frontend/src/homepage.js
@@ -6,12 +6,15 @@ class HomePage extends React.Component {
     return (
       <div>
         <div style={{display:"flex"}}>
-        <img src='/cardealership.jpeg' style={{width: "300px"}}/>
+        <figure class="figure">
+            <img src="/cardealership.jpeg" style={{width: "300px"}} class="figure-img img-fluid rounded" alt="Our dealership in New York"></img>
+            <figcaption class="figure-caption">Our Dealership in New York </figcaption>
+        </figure>
         <div style={{margin:"100px"}}>
              <span style={{margin:"60px"}}>
-             "We are proud to offer you the best car dealership in the USA"
+             We are proud to offer you <mark>the best car dealership</mark>in the USA!
                </span >
-           </div>
+        </div>
 
         </div>
         
