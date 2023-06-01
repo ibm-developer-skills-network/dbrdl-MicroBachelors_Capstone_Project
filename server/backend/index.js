30de3fad0da0805091ee6f79911b2354617d6d94
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-06-01 09:43:41 +0530

    Update index.js

--- server/backend/index.js
+++ server/backend/index.js
@@ -5,7 +5,7 @@ const app = new express();
 //Adding a middleware to set the path for all static pages
 app.use(express.static('pages'))
 
-const PORT = process.env.PORT || 8000; 
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`)
